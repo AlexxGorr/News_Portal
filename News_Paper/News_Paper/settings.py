4013d96928256922ad0f87fffbcae14723d9d323
AlexxGorr <infernal.mail@gmail.com>
2023-01-26 20:37:41 +0500

    NP logging

--- News_Paper/News_Paper/settings.py
+++ News_Paper/News_Paper/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import logging
+
+
+logger = logging.getLogger('django')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -198,6 +202,133 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'style': '{',
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s %(levelname)s %(message)s',
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple_warning': {
+            'format': '%(pathname)s'
+        },
+        'simple_error': {
+            'format': '%(exc_info)s'
+        },
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s %(module)s %(message)s',
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'verbose_': {
+            'format': '%(asctime)s %(levelname)s %(pathname)s %(message)s %(exc_info)s',
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'verbose_email': {
+            'format': '%(asctime)s %(levelname)s %(message)s %(pathname)s',
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'verbose_security': {
+            'format': '%(asctime)s %(levelname)s %(module)s %(message)s',
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+
+    },
+
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        }
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'console_warning': {
+            'level': 'WARNING',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple_warning'
+        },
+        'console_error': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple_error'
+        },
+
+        'general': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': 'general.log',
+            'formatter': 'verbose',
+        },
+        'errors': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': 'errors.log',
+            'formatter': 'verbose_'
+        },
+        'security': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'security.log',
+            'formatter': 'verbose_security'
+        },
+
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'verbose_email'
+        },
+
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'console_warning', 'console_error', 'general'],
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['errors', 'mail_admins'],
+            'propagate': True,
+        },
+        'django.server': {
+            'handlers': ['errors', 'mail_admins'],
+            'propagate': True,
+        },
+        'django.template': {
+            'handlers': ['errors'],
+            'propagate': True,
+        },
+        'django.db_backends': {
+            'handlers': ['errors'],
+            'propagate': True,
+        },
+        'django.security': {
+            'handlers': ['security'],
+            'propagate': True,
+        },
+
+    },
+}
+
+
+
+
+
+
 
 
 
--- News_Paper/news/admin.py
+++ News_Paper/news/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Authors)
-admin.site.register(Categories)
-admin.site.register(Posts)
-admin.site.register(Post_Category)
-admin.site.register(Comments)
+
+def nullfy_posts(modeladmin, request, queryset):
+    queryset.update(rate_post = 0)
+nullfy_posts.short_description = 'Обнулить рейтинг'
+
+class Post_CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'posts', 'categories')
+    list_filter = ['categories']
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'authors', 'post_type', 'date_create', 'header', 'body', 'rate_post')
+    list_filter = ['date_create']
+    search_fields = ('authors', 'header')
+    actions = [nullfy_posts]
+
+class AuthorsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'rating', 'user')
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'cat_name')
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'date_create', 'text_comment', 'rate_comment', 'posts', 'user')
+
+
+
+admin.site.register(Authors, AuthorsAdmin)
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Posts, PostsAdmin)
+admin.site.register(Post_Category, Post_CategoryAdmin)
+admin.site.register(Comments, CommentsAdmin)
 
